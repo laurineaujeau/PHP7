8d66cdbb9d4407dd91c0869df6ea1d706e6353fb
lauri <laurine.aujeau@isen-ouest.yncrea.fr>
2020-04-02 17:18:45 +0200

    v2

--- tp7.php
+++ tp7.php
@@ -22,6 +22,7 @@ class form2 extends Formulaire {
         echo'<input type=\'radio\' name=\'genre\' value=\'$FH\'/>';
     }
 }
+echo '<strong>Exercice 1</strong><br>';
 $f=new form2();
 $f->__construct();
 echo 'Votre nom :';
@@ -44,4 +45,74 @@ echo'<br>';
 echo 'Equitation ';
 $f->ajouterBox("Equitation");
 $f->getform();
+
+echo '<br><br><strong>Exercice 2</strong><br>';
+/*interface Shape{
+    function getArea();
+}*/
+abstract class Shape{
+    abstract function getArea();
+}
+//class Square implements Shape{
+class Square extends Shape{
+    private $width;
+    private $height;
+    function getArea(){echo "25 ";}
+
+}
+//class Circle implements Shape{
+class Circle extends Shape{
+    private $radius;
+    function getArea(){echo "153.86 ";}
+
+}
+$s=new Square();
+$c=new Circle();
+$tab=array($s,$c);
+
+for($i=0;$i<count($tab);$i++){ // fonctione existente qui calcule la longueur d'un tableau ou d'une chaine de caractère, renvoie un int
+    echo get_class($tab[$i]); // fonction existente qui renvoie un string
+    echo " Area : ";
+    echo $tab[$i]->getArea();
+    echo'<br>';
+}
+echo '<br><br><strong>Exercice 3</strong><br>';
+Trait Un{
+    function small($text){
+        echo '<small>'.$text.'</small>';
+    }
+    function big($text){
+        echo '<h4>'.$text.'</h4>';
+    }
+
+}
+Trait Deux{
+    function small($text){
+        echo '<i>'.$text.'</i>';
+    }
+    function big($text){
+        echo '<h2>'.$text.'</h2>';
+    }
+}
+class Text{
+    use Un,Deux{
+        Un::small insteadof Deux;
+                                // affiche small de un
+        Deux::big insteadof Un;// affiche big de Deux
+        Deux::big as gros; // as remplace le nom de la fonction
+                            // affiche big de Deux
+        Un::small as petit;// affiche small de un
+
+    }
+
+}
+$t= new Text();
+$t->small("Banane");
+
+$t->big("Banane");
+
+$t->gros("Banane");
+
+$t->petit("Banane");
+
 ?>
